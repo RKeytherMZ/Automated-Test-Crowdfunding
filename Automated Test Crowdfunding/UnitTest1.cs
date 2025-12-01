eda8b51b62a917011695eb69d33ffe8f4b31b6e9
RKeytherMZ <rubbykeyther@gmail.com>
2025-12-01 03:19:26 -0400

    Add Nugets Selenium

---
+++ Automated Test Crowdfunding/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Automated_Test_Crowdfunding
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
